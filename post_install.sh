14797146b83f112186669ba8ba08fa30ac33008e
Angel Alonso <angel.alonso@accenture.com>
2020-03-19 15:29:30 +0100

    Modified overlay and added php packages

--- post_install.sh
+++ post_install.sh
@@ -10,7 +10,7 @@ sysrc -f /etc/rc.conf nginx_enable="YES"
 sysrc -f /etc/rc.conf mysql_enable="YES"
 sysrc -f /etc/rc.conf php_fpm_enable="YES"
 
-# Setup php-fpm 
+# Setup php-fpm
 cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
 sed -i '' -e 's?listen = 127.0.0.1:9000?listen = /var/run/php-fpm.sock?g' /usr/local/etc/php-fpm.d/www.conf
 sed -i '' -e 's/;listen.owner = www/listen.owner = www/g' /usr/local/etc/php-fpm.d/www.conf
@@ -35,8 +35,14 @@ GRANT ALL PRIVILEGES ON bookstack.* TO 'bookstack'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
+# Install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
 # Install bookstack
-cd /usr/local/bookstack
+cd /usr/local/bookstack || exit
 composer install
 cp .env.example .env
 
